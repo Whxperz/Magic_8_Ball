595cda58464aa730f2bbad632d1e7f31c2adc8ec
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-09-13 14:17:44 -0400

    Started Magic 8 Ball Project

---
+++ src/Magic8Ball.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[]args){
+            Scanner keyboard;
+   //Responses
+
+        String firstResponse = "Most likely";
+        String secondResponse = "Outlook good";
+        String thirdResponse = "Without a doubt";
+        String fourthResponse = "Yes";
+        String fifthResponse = "It is certain";
+        String sixthResponse = "My reply is no";
+        String seventhResponse = "Don't count on it";
+        String eigthResponse = "Outlook not so good";
+        String ninthResponse = "My sources say no";
+
+  //Integers
+
+    int firstNumber = 0;
+    int secondNumber = 11;
+    int thirdNumber = 21;
+    int fourthNumber = 31;
+    int fifthNumber = 41;
+    int sixthNumber = 51;
+    int seventhNumber = 61;
+    int eighthNumber = 71;
+    int ninthNumber = 81;
+    int tenthNumber = 91;
+    int eleventhNumber = 100;
+        if(firstNumber>secondNumber){
+            System.out.println();
+        }
+
+
+    }
+}
