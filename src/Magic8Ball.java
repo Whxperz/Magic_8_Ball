38f9325a9979b48e0235b775eb6158cd6228e8d7
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-09-17 13:34:59 -0400

    Magic 8 Ball Rough Draft

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -4,6 +4,7 @@ public class Magic8Ball {
 
     public static void main(String[]args){
             Scanner keyboard;
+            keyboard = new Scanner(System.in);
    //Responses
 
         String firstResponse = "Most likely";
@@ -15,24 +16,62 @@ public class Magic8Ball {
         String seventhResponse = "Don't count on it";
         String eigthResponse = "Outlook not so good";
         String ninthResponse = "My sources say no";
+        String tenthResponse = "Doubtful";
 
   //Integers
 
-    int firstNumber = 0;
-    int secondNumber = 11;
-    int thirdNumber = 21;
-    int fourthNumber = 31;
-    int fifthNumber = 41;
-    int sixthNumber = 51;
-    int seventhNumber = 61;
-    int eighthNumber = 71;
-    int ninthNumber = 81;
-    int tenthNumber = 91;
-    int eleventhNumber = 100;
-        if(firstNumber>secondNumber){
-            System.out.println();
+        int number;
+        int firstNumber = 1;
+        int rangeDistance = 10;
+
+
+        int secondNumber = firstNumber + rangeDistance;
+        int thirdNumber = secondNumber+rangeDistance;
+        int fourthNumber = thirdNumber+rangeDistance;
+        int fifthNumber = fourthNumber+rangeDistance;
+        int sixthNumber = 51;
+        int seventhNumber = 61;
+        int eighthNumber = 71;
+        int ninthNumber = 81;
+        int tenthNumber = 91;
+        int eleventhNumber = 100;
+
+
+   //Magic 8 Ball
+
+        System.out.println("Pick a number between 1 and 100");
+        number = keyboard.nextInt();
+        if(firstNumber>=number){
+            System.out.println(fourthResponse);
+            }
+        else if(secondNumber>number) {
+            System.out.println(secondResponse);
+            }
+        else if(thirdNumber>number) {
+            System.out.println(thirdResponse);
         }
-
-
+        else if(fourthNumber>number) {
+            System.out.println(firstResponse);
+        }
+        else if(fifthNumber>number) {
+            System.out.println(secondResponse);
+        }
+        else if(sixthNumber>number) {
+            System.out.println(fifthResponse);
+        }else if(seventhNumber>number) {
+            System.out.println(sixthResponse);
+        }
+        else if(eighthNumber>number) {
+            System.out.println(ninthResponse);
+        }
+        else if(ninthNumber>number) {
+            System.out.println(tenthResponse);
+        }
+        else if(tenthNumber>number) {
+            System.out.println(secondResponse);
+        }
+        else if(eleventhNumber>=number) {
+            System.out.println(seventhResponse);
+        }
     }
 }
